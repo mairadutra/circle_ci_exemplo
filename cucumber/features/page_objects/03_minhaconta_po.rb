16477585218931f63543d3d97b53d47a9f2499fd
mairadutra <maira.dutra>
2017-04-04 14:54:00 -0300

    ajuste circle ci

--- cucumber/features/page_objects/03_minhaconta_po.rb
+++
@@ -1,8 +0,0 @@
-class PaginaMinhaConta< SitePrism::Page
-
-    #element :titulo, :xpath, "//select[@name='gender']"
-    element :nomerua, "#streetAddress1"
-    element :numero, "#streetNumber"
-    element :telefone, "#phoneNumber"
-    element :bairro, "#district"
-end
